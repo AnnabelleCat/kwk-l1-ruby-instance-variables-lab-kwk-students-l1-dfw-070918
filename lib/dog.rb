1a7be39bf3de5345e437546b7f2c38d00b2bd96e
AnnabelleCat <pikainka@gmail.com>
2018-07-12 21:05:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 # dog.rb
 
-class Dog
-  def name=(dog_name)
-    @this_dogs_name = dog_name
+class Dog     #create class
+  def name=(dog_name)   #define method
+    @this_dogs_name = dog_name     #make dog_name variable an instance variable
   end
   
-  def name
+  def name   #getter method to be able to retrieve name
     @this_dogs_name
   end
 end
